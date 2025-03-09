c4f53fa2e70c5ecd5f4a96f1159998c97b6fe9f0
Paul-Elie Pipelin <paul.elie97@gmail.com>
2025-03-09 21:51:19 +0100

    tmp

--- src/interface.zig
+++ src/interface.zig
@@ -1,4 +1,5 @@
 const position = @import("position.zig");
+const search = @import("search.zig");
 const std = @import("std");
 const types = @import("types.zig");
 
@@ -90,6 +91,18 @@ pub fn loop(stdin: anytype, stdout: anytype) !void {
             }
         }
 
+        if (std.mem.eql(u8, "go", primary_token)) {
+            existing_command = true;
+            const tmp_position: position.Position = pos;
+            const tmp_states: StateList = try states.clone(allocator);
+            if (cmd_go(stdout, &pos, &tokens, &states)) {} else |err| {
+                try stdout.print("Command go failed with error {}\n", .{err});
+                pos = tmp_position;
+                states = tmp_states;
+                pos.state = &states.items[states.items.len - 1];
+            }
+        }
+
         if (std.mem.eql(u8, "isready", primary_token)) {
             existing_command = true;
             try stdout.print("readyok\n", .{});
@@ -173,5 +186,59 @@ fn cmd_position(pos: *position.Position, tokens: anytype, states: *StateList) !v
             states.append(allocator, position.State{}) catch unreachable;
             try pos.movePiece(try types.Move.initFromStr(pos.*, token.?), &states.items[states.items.len - 1]);
         }
+    }
+}
+
+fn cmd_go(stdout: anytype, pos: *position.Position, tokens: anytype, states: *StateList) !void {
+    _ = states;
+    var limits: types.Limits = types.Limits{};
+    var token: ?[]const u8 = tokens.next();
+    g_stop = false;
+
+    limits.start = types.now();
+
+    while (token != null) : (token = tokens.next()) {
+        // Needs to be the last command on the line
+        if (std.mem.eql(u8, "searchmoves", token.?)) {
+            // TODO
+            break;
+        } else if (std.mem.eql(u8, "wtime", token.?)) {
+            limits.time[types.Color.white.index()] = try std.fmt.parseInt(types.TimePoint, tokens.next().?, 10);
+        } else if (std.mem.eql(u8, "btime", token.?)) {
+            limits.time[types.Color.black.index()] = try std.fmt.parseInt(types.TimePoint, tokens.next().?, 10);
+        } else if (std.mem.eql(u8, "winc", token.?)) {
+            limits.inc[types.Color.white.index()] = try std.fmt.parseInt(types.TimePoint, tokens.next().?, 10);
+        } else if (std.mem.eql(u8, "binc", token.?)) {
+            limits.time[types.Color.black.index()] = try std.fmt.parseInt(types.TimePoint, tokens.next().?, 10);
+        } // else if (std.mem.eql(u8, "movestogo", token.?)) {
+        //     limits.movestogo = try std.fmt.parseInt(u8, tokens.next().?, 10);
+        // }
+        else if (std.mem.eql(u8, "depth", token.?)) {
+            limits.depth = try std.fmt.parseInt(u8, tokens.next().?, 10);
+        } // else if (std.mem.eql(u8, "nodes", token.?)) {
+        //     limits.nodes = try std.fmt.parseInt(u32, tokens.next().?, 10);
+        // }
+        else if (std.mem.eql(u8, "movetime", token.?)) {
+            limits.movetime = try std.fmt.parseInt(types.TimePoint, tokens.next().?, 10);
+        } // else if (std.mem.eql(u8, "mate", token.?)) {
+        //     limits.mate = try std.fmt.parseInt(types.TimePoint, tokens.next().?, 10);
+        // }
+        else if (std.mem.eql(u8, "perft", token.?)) {
+            limits.perft = try std.fmt.parseInt(u8, tokens.next().?, 10);
+        } else if (std.mem.eql(u8, "infinite", token.?)) {
+            limits.infinite = true;
+        } // else if (std.mem.eql(u8, "ponder", token.?)) {
+        // }
+    }
+
+    var t = try std.time.Timer.start();
+    if (limits.perft > 0) {
+        const nodes = try search.perft(allocator, stdout, pos, limits.perft, true);
+        const nodes_f: f64 = @floatFromInt(nodes);
+        const time_f: f64 = @floatFromInt(t.read());
+
+        try stdout.print("info nodes {} time {} ({d:.1} Mnps)\n", .{ nodes, std.fmt.fmtDuration(t.read()), (nodes_f / (time_f / 1000.0)) });
+    } else {
+        try stdout.print("limits {}", .{limits});
     }
 }
